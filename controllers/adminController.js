7f140b6f9428c584e9c94cdedfe79248d90bb08f
Thelma101 <akpata.thelma@gmail.com>
2025-03-27 18:15:28 +0100

    admin protected route configuration

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -13,6 +13,30 @@ const getAddProduct = async (req, res) => {
             stack: stack.error
         })
     }
+};
+
+const addProduct = async (req, res) => {
+    try {
+        const { title, price, description, imageUrl } = req.body;
+        const product = new Product({
+            title,
+            price,
+            description,
+            imageUrl,
+            userId: req.user._id
+        });
+        await product.save();
+        res.redirect('/admin/products');
+    } catch (error) {
+        console.log(error);
+        res.render("error", {
+            pageTitle: "Error",
+            path: "/error",
+            error: error,
+            message: "Something went wrong. Please try again later.",
+            stack: stack.error
+        })
+    }
 }
 
 module.exports = {
