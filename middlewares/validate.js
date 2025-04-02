7b855ba6a567f38751bb01ecbbd582a3dd93eda7
Thelma101 <akpata.thelma@gmail.com>
2025-04-02 07:06:43 +0100

    Joi input validate

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -2,23 +2,23 @@ const Joi = require('joi');
 
 const productValidation = Joi.object({
     title: Joi.string().required().message({
-        'string.empty': "Product title is required"
+        "string.empty": "Product title is required"
     }),
     description: Joi.string().required().message({
-        'string.empty': "Product description is required"
+        "string.empty": "Product description is required"
     }),
     category: Joi.string().required().message({
         "string.empty": "Product category is required"
     }),
-    color: Joi.string().required().message({
+    color: Joi.any().required().message({
         "string.empty": "Product color is required"
     }),
-    price: Joi.number().required().message({
-        'number.empty': "Product price is required",
+    price: Joi.number().min(0).required().message({
+        "number.empty": "Product price is required",
         "number.min": "Product price must be greater than 0",
         "any.required": "Product price is required"
     }),
-    stock: Joi.number().min().required().message({
+    stock: Joi.number().min(1).required().message({
         "string.empty": "Product stock is required",
         "number.min": "Product stock must be greater than 1",
         "any.required": " Stock is required"
